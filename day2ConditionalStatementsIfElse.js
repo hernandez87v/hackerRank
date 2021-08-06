dde403e3a1dffe17becef820f358261b48bdcb51
hernandez87v <hernandez87v@hotmail.com>
2021-08-06 14:38:37 -0700

    solve why original solution wasnt working 🔍

--- day2ConditionalStatementsIfElse.js
+++ day2ConditionalStatementsIfElse.js
@@ -1,20 +1,21 @@
 // HACKERRANK > 10 Days of Javascript > Day 2: Conditional Statements: If-Else
 
+// Original Solution
 // function getGrade(score) {
 //   let grade;
 //   // Write your code here
-//   if (score < 25 && score <= 30) {
-//     return 'A';
-//   } else if (score < 20 && score <= 25) {
-//     return 'B';
-//   } else if (score < 15 && score <= 20) {
-//     return 'C';
-//   } else if (score < 10 && score <= 15) {
-//     return 'D';
-//   } else if (score < 5 && score <= 10) {
-//     return 'E';
-//   } else if (score <= 0 && score <= 5) {
-//     return 'F';
+//   if (score > 25 && score <= 30) {
+//     grade = 'A';
+//   } else if (score > 20 && score <= 25) {
+//     grade = 'B';
+//   } else if (score > 15 && score <= 20) {
+//     grade = 'C';
+//   } else if (score > 10 && score <= 15) {
+//     grade = 'D';
+//   } else if (score > 5 && score <= 10) {
+//     grade = 'E';
+//   } else if (score >= 0 && score <= 5) {
+//     grade = 'F';
 //   }
 //   return grade;
 // }
