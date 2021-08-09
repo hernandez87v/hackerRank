c296f9da6f72b85e0e513bcc8937f44b46e9ef3b
hernandez87v <hernandez87v@hotmail.com>
2021-08-09 14:59:55 -0700

    solution✅ to Day 3: Arrays

--- day3Arrays.js
+++ day3Arrays.js
@@ -1,1 +1,27 @@
 // HACKERRANK > 10 Days of Javascript > Day 3: Arrays
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+
+const nums = [2, 3, 6, 6, 5];
+
+function getSecondLargest(nums) {
+  // Complete the function
+  let max = -Infinity,
+    result = -Infinity;
+
+  for (const value of nums) {
+    const nr = Number(value);
+
+    if (nr > max) {
+      [result, max] = [max, nr]; // save previous max
+    } else if (nr < max && nr > result) {
+      result = nr; // new second biggest
+    }
+  }
+  return result;
+}
+
+console.log(getSecondLargest(nums)); // 5
