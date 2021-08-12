d8fab92ba8e827d274c0ff063b1340c6c0f7d5ec
hernandez87v <hernandez87v@hotmail.com>
2021-08-12 14:27:02 -0700

    solution to Day 4: Create a Rectangle Object

--- day4CreateARectangleObject.js
+++ day4CreateARectangleObject.js
@@ -1,1 +1,19 @@
 // HACKERRANK > 10 Days of Javascript > Day 4: Create a Rectangle Object
+/*
+ * Complete the Rectangle function
+ */
+// let a = 4;
+// let b = 5;
+
+function Rectangle(a, b) {
+  var obj = {
+    length: a,
+    width: b,
+    perimeter: 2 * (a + b),
+    area: a * b,
+  };
+
+  return obj;
+}
+
+console.log(Rectangle(4, 5));
