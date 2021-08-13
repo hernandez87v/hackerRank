a49d1f5ba2f06ced74d9f6e9c8ac483acc77b345
hernandez87v <hernandez87v@hotmail.com>
2021-08-13 15:33:02 -0700

    solution ✅ Day 4: Count Objects

--- day4CountObjects.js
+++ day4CountObjects.js
@@ -1,1 +1,30 @@
 // HACKERRANK > 10 Days of Javascript > Day 4: Count Objects
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+let objects = [
+  { x: 1, y: 1 },
+  { x: 2, y: 3 },
+  { x: 3, y: 3 },
+  { x: 3, y: 4 },
+  { x: 4, y: 5 },
+];
+
+function getCount(objects) {
+  let n = 0;
+
+  for (let o of objects) {
+    n += o.x == o.y;
+  }
+  return n;
+}
+
+console.log(getCount(objects));
+
+// Cleaner solution using reduce()-------------------------------------
+// function getCount(objects) {
+//   return objects.reduce((count, o) => count + (o.x == o.y), 0);
+// }
