433ef5d9af6a37bbfe5c819559da7111b9ca9b12
hernandez87v <hernandez87v@hotmail.com>
2021-09-01 13:45:03 -0700

    Solve - day 4 Classes

--- day4Classes.js
+++ day4Classes.js
@@ -1,1 +1,14 @@
 // HACKERRANK > 10 Days of Javascript > Day 4: Classes
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;
+  }
+  perimeter() {
+    return this.sides.reduce((x, y) => x + y);
+  }
+}
